3e4c052bdfee5117558db1e34da09303873058ce
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-03-18 09:54:03 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (100.00%) - LeetHub

---
+++ jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size(), INT_MAX);
+        dp[0]=0;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=1;j<=nums[i];j++){
+                if(i+j>=nums.size())
+                    break;
+                dp[i+j]=min(dp[i+j], dp[i]+1);
+            }
+        }
+        return dp[nums.size()-1];
+    }
+};
