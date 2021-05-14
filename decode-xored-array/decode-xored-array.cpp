699fed2e6508d1914cee05e7d5d6c38225370318
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-05-14 23:21:46 +0530

    Time: 20 ms (94.86%), Space: 25.9 MB (48.06%) - LeetHub

---
+++ decode-xored-array/decode-xored-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        
+        vector<int> res;
+        
+        res.push_back(first);
+        
+        for(int i=0;i<encoded.size();i++)
+            res.push_back(encoded[i]^res[i]);
+        
+        return res;
+    }
+};
