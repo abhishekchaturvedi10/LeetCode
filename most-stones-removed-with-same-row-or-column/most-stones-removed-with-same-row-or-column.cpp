dd8158ec51af04df3ab904ce7eab2a7c179876b7
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-05-18 00:08:08 +0530

    Time: 52 ms (65.43%), Space: 21.5 MB (31.26%) - LeetHub

---
+++ most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> parent;
+    
+    int find(int i){
+        if(parent[i]==i)
+            return i;
+        return parent[i]=find(parent[i]);
+    }
+    
+    void Union(int x, int y){
+        parent[find(x)]=find(y);
+    }
+    
+    int removeStones(vector<vector<int>>& stones) {
+        
+        for(int i=0;i<stones.size();i++){
+            parent.push_back(i);
+        }
+        
+        map<int,vector<int>> row, column;
+        
+        for(int i = 0; i < stones.size(); i++){
+            row[stones[i][0]].push_back(i);
+            column[stones[i][1]].push_back(i);
+        }
+        
+        for(int i = 0; i < stones.size(); i++) {
+		    for(int j : row[stones[i][0]]) Union(i, j);
+		    for(int j : column[stones[i][1]]) Union(i, j);
+	    }
+
+	    unordered_set<int> unique;
+        
+	    for(int n : parent) 
+            unique.insert(find(n));
+        
+	    return stones.size() - unique.size();
+    }
+};
