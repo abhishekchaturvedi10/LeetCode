ef8a51359651d5c55d31439287d3038eb8954c3e
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-05-17 13:19:13 +0530

    Time: 12 ms (32.79%), Space: 15.4 MB (50.87%) - LeetHub

---
+++ validate-stack-sequences/validate-stack-sequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        
+        stack<int> s;
+        
+        int j=0;
+        
+        for(int i:pushed){
+            s.push(i);
+            while(!s.empty()&&s.top()==popped[j]){
+                s.pop();
+                j++;
+            }
+        }
+        
+        return s.empty();
+    }
+};
