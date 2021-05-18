92e981fe511a19f219f79cbce16ec7b2231274b0
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-05-18 22:59:08 +0530

    Time: 24 ms (25.29%), Space: 8.7 MB (42.75%) - LeetHub

--- stone-game/stone-game.cpp
+++ stone-game/stone-game.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
+    
     int dp[500][500];
     
-    int game(vector<int>& a, int lo, int hi){
-        if(lo > hi) return 0;
-        if(dp[lo][hi] != -1) 
-            return dp[lo][hi];
-        return dp[lo][hi] = max(a[lo] + min(game(a, lo + 2, hi), game(a, lo + 1, hi - 1)), a[hi] + min(game(a, lo + 1, hi - 1), game(a, lo, hi - 2)));
+    int game(vector<int> &arr, int s, int e){
+        
+        if(s>e)
+            return 0;
+        
+        if(dp[s][e]!=-1)
+            return dp[e][s];
+        
+        return dp[s][e]=max({arr[s]+game(arr,s+2,e), arr[s]+game(arr,s+1,e-1), arr[e]+game(arr,s+1,e-1), arr[e]+game(arr,s,e-2)});
     }
     
     bool stoneGame(vector<int>& piles) {
-        memset(dp, -1, sizeof dp);
-        return (game(piles, 0, piles.size() - 1) > 0);
+        
+        memset(dp, -1, sizeof(dp));
+        
+        int Alex = game(piles, 0, piles.size()-1);
+        
+        int total=0;
+        
+        for(int i:piles)
+            total+=i;
+        
+        int Lee = total-Alex;
+        
+        return Alex>Lee;
     }
 };
