508d700c3a2101e365ee38afd934370b48843a51
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-05-20 17:53:33 +0530

    Time: 8 ms (59.32%), Space: 10.3 MB (18.44%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -4,25 +4,16 @@ public:
         
         int n=a.size(),l=0,r=n-1;
                 
-        while(r>=l){
+        while(r>l){
             
             int m=l+(r-l)/2;
             
-            if(m<n-1&&a[m]>a[m+1]){
-                return a[m+1];
-                break;
-            }
-            
-            if(m>0&&a[m-1]>a[m]){
-                return a[m];
-            }
-            
-            if(a[l]>a[m])
-                r=m-1;
-            else
+            if(a[r]<a[m])
                 l=m+1;
+            else
+                r=m;
         }
         
-        return a[0];
+        return a[l];
     }
 };
