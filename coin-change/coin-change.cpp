bd895fe909647f46f9f595ef727466ff0d6a48e6
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-05-21 22:57:18 +0530

    Time: 44 ms (94.72%), Space: 11.5 MB (72.04%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
+    
     int dp[10005];
+    
     int fun(int amt,vector<int>& coins){
-        if(amt==0){
+        
+        if(amt==0)
             return 0;
-        }
-        if(dp[amt]!=-1){
+        
+        if(dp[amt]!=-1)
             return dp[amt];
-        }
+        
         dp[amt]=10005;
+        
         for(int i:coins){
-            if(i>amt){
+            
+            if(i>amt)
                 continue;
-            }
+            
             dp[amt]=min(1+fun(amt-i,coins),dp[amt]);
         }
+        
         return dp[amt];
     }
+    
     int coinChange(vector<int>& coins, int amount) {
+        
         memset(dp,-1,sizeof(dp));
+        
         int x = fun(amount, coins);
-        if(x>10000){
+        
+        if(x>10000)
             return -1;
-        }
+        
         return x;
     }
 };
