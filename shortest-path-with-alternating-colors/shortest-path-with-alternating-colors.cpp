8c7d6f5c5c440bc05b01339e9304cd812b789f2e
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-05-21 20:16:11 +0530

    Time: 20 ms (65.37%), Space: 14.6 MB (88.66%) - LeetHub

---
+++ shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    
+    vector<int> res1, res2;
+    
+    vector<vector<int>> rg, bg;
+    
+    void dfs(int u, int prev, int dist){
+                         
+        if(prev==0&&res1[u]<dist)
+            return;
+        
+        if(prev==1&&res2[u]<dist)
+            return;
+        
+        if(prev==0)
+            res1[u]=min(res1[u],dist);
+        else
+            res2[u]=min(res2[u],dist);
+        
+        if(prev==0)
+            for(int i:bg[u])
+                dfs(i, 1, dist+1);
+        else
+            for(int i:rg[u])
+                dfs(i, 0, dist+1);
+    }
+    
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& red_edges, vector<vector<int>>& blue_edges) {
+        
+        rg.resize(n);
+        bg.resize(n);
+        
+        for(auto const &edge:red_edges)
+            rg[edge[0]].push_back(edge[1]);
+        
+        for(auto const &edge:blue_edges)
+            bg[edge[0]].push_back(edge[1]);
+        
+        res1.assign(n, INT_MAX);
+        res2.assign(n, INT_MAX);
+        
+        dfs(0, 0, 0);
+        dfs(0, 1, 0);
+        
+        vector<int> res(n);
+        
+        for(int i=0;i<n;i++){
+            if(res1[i]==INT_MAX&&res2[i]==INT_MAX)
+                res[i]=-1;
+            else
+                res[i]=min(res1[i],res2[i]);
+        }
+        
+        return res;
+    }
+};
