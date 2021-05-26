ac8ca27918db27441844815cf2764c0e67bbbf55
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-05-26 18:09:23 +0530

    Time: 32 ms (68.90%), Space: 32.7 MB (70.68%) - LeetHub

---
+++ delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* dfs(TreeNode *root, int key){
+                
+        if(!root)
+            return NULL;
+        
+        if(root->val==key){
+                        
+            TreeNode* dummy=new TreeNode(0);
+            
+            dummy->right=root;
+            
+            if(!root->left&&!root->right)
+                return NULL;
+            
+            else if(!root->right)
+                return root->left;
+            
+            else if(!root->left)
+                return root->right;
+            
+            else{
+                
+                TreeNode *temp=root->left;
+                TreeNode *temp1=root->right;
+                
+                root=temp;
+                
+                dummy->right=root;
+                
+                while(root->right)
+                    root=root->right;
+                
+                root->right=temp1;
+                
+                return dummy->right;
+            }
+        }
+        
+        root->left=dfs(root->left,key);
+        root->right=dfs(root->right,key);
+        
+        return root;
+    }
+    
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        
+        return dfs(root,key);
+    }
+};
