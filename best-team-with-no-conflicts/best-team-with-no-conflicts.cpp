a4d71eef48682f9e1141d8f3ca977702832970d4
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-05-31 20:37:21 +0530

    Time: 324 ms (11.91%), Space: 21.7 MB (23.20%) - LeetHub

--- best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
+++ best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
@@ -14,23 +14,17 @@ public:
         vector<int> dp(n);
         
         dp[0]=players[0][1];
-        
-        int res=dp[0];
-        
+                
         for(int i=1;i<n;i++){
             
             dp[i]=players[i][1];
             
-            for(int j=0;j<i;j++){
-                
-                if(players[i][1]>=players[j][1]){
+            for(int j=0;j<i;j++)
+                if(players[i][1]>=players[j][1])
                     dp[i]=max(dp[j]+players[i][1], dp[i]);
-                }
-            }
             
-            res=max(res,dp[i]);
         }
         
-        return res;
+        return *max_element(dp.begin(), dp.end());
     }
 };
