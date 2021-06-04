d1da8297b54d7e9ab9812c2af2685f9800a80198
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-06-04 19:35:20 +0530

    Time: 120 ms (24.34%), Space: 41.7 MB (24.09%) - LeetHub

---
+++ minimum-jumps-to-reach-home/minimum-jumps-to-reach-home.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
+        
+        unordered_map<int,int> s, vis1,vis2;
+                
+        for(int i:forbidden)
+            s[i]=1;
+        
+        if(s[0]||s[a])
+            return -1;
+        
+        vis1[0]=1;
+        
+        queue<vector<int>> q;
+        
+        q.push({0,1});
+        
+        // vis1[a]=1;
+        
+        int steps=0;
+        
+        while(!q.empty()){
+            
+            int len=q.size();
+            
+            while(len--){
+                
+                auto v=q.front();
+                
+                q.pop();
+                
+                if(v[0]==x)
+                    return steps;
+                
+                // cout<<v[0]<<" "<<steps<<'\n';
+                
+                if(!s[v[0]+a]&&!vis1[v[0]+a]&&(v[0]+a<=6000)){
+                    vis1[v[0]+a]=1;
+                    q.push({v[0]+a,1});
+                }
+                    
+                if(v[0]-b<0||v[1]==2)
+                    continue;
+                    
+                if(!s[v[0]-b]&&!vis2[v[0]-b]){
+                    vis2[v[0]-b]=1;
+                    q.push({v[0]-b,2});
+                }
+            }
+            
+            steps++;
+        }
+        
+        return -1;
+    }
+};
