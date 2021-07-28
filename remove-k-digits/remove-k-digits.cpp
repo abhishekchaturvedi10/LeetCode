ccb631741ce0cad512eaf70eea3772c2657a332b
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-07-28 23:32:48 +0530

    Time: 4 ms (90.07%), Space: 6.9 MB (86.04%) - LeetHub

---
+++ remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        
+        string res="";
+        
+        for(char &c:num) {
+            
+            while(k&&res.size()&&res.back()>c){
+                
+                res.pop_back();
+                k--;
+            }
+            
+            if(res.size()||c!='0')
+                res.push_back(c);
+        }
+        
+        while(res.size()&&k--)
+            res.pop_back();
+        
+        if(res=="")
+            return "0";
+        
+        return res;
+    }
+};
