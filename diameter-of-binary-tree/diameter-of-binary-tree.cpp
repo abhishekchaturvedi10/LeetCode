b0015a82daf21696152bd5ef76783dabe1490dd0
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-08-03 21:14:35 +0530

    Time: 0 ms (100.00%), Space: 20.1 MB (92.29%) - LeetHub

--- diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,34 +11,31 @@
  */
 class Solution {
 public:
+    
     int ans;
-    int dfs(TreeNode* root){
-        if(!root){
-            return -1;
-        }
-        if(!root->left&&!root->right){
-            return 0;
-        }
-        int l=0,r=0;
-        if(root->left&&root->right){
-            l=1+dfs(root->left);
-            r=1+dfs(root->right);
-            ans=max(ans,l+r);
-            return max(l,r);
-        }
-        if(!root->left&&root->right){
-            r=1+dfs(root->right);
-            ans=max(ans,r);
-            return r;
-        }
-        l=1+dfs(root->left);
-        ans=max(ans,l);
-        return l;
-    }
-    int diameterOfBinaryTree(TreeNode* root) {
+    
+    int height(TreeNode* root){
+        
         if(!root)
             return 0;
+        
+        int l = height(root->left);
+        int r = height(root->right);
+        
+        ans=max(ans, l+r);
+        
+        return 1+max(l,r);
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        
+        if(!root)
+            return 0;
+        
         ans=0;
-        return max(ans, 2+dfs(root->left)+dfs(root->right));
+        
+        height(root);
+        
+        return ans;
     }
 };
