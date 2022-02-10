c34b2c1ef9e6e5294fd9e081ae3d4dad92e07222
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2022-02-11 00:44:24 +0530

    Time: 23 ms (61.04%), Space: 13.4 MB (37.23%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -5,21 +5,19 @@ public:
         if(nums.size()==1)
             return 0;
         
-        map<int, int> m;
+        unordered_map<int, int> m;
                 
         for(int i:nums)
             m[i]++;
         
         int res=0;
                 
-        for(auto i:m) {
-            if(m.find(i.first+k)!=m.end()){
-                if(k==0&&m[i.first]>1)
-                    res++;
+        for(auto &i:m) {
+            if(k==0&&m[i.first]>1)
+                res++;
                 
-                if(k!=0)
-                    res++;
-            }
+            if(k!=0&&m.find(i.first+k)!=m.end())
+                res++;
         }
         
         return res;
