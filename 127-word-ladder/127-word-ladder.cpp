a3bbe4adb4e291cc7eabb4da822c0dc910bc8e8b
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2022-02-13 22:08:03 +0530

    Time: 1612 ms (7.84%), Space: 17.3 MB (33.08%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        int res=0;
+        
+        queue<string> q;
+        
+        q.push(beginWord);
+        
+        map<string,int> seen;
+        
+        while(q.size()>0) {
+            
+            res++;
+            
+            int n=q.size();
+            
+            while(n--) {
+                
+                string s=q.front();
+                
+                q.pop();    
+                
+                if(s==endWord)
+                    return res;
+                
+                if(seen[s])
+                    continue;
+                
+                seen[s]++;
+                
+                for(auto ss:wordList) {
+                    if(ss.size()==s.size()) {
+                        int cnt=0;
+                        for(int i=0;i<ss.size();i++) {
+                            if(ss[i]!=s[i]){
+                                cnt++;
+                            }
+                        }
+                        if(cnt==1)
+                            q.push(ss);
+                    }
+                }
+            }
+        }
+        
+        return 0;
+    }
+};
