fc624d4e8d9bd1698b75af660e37f443bc084368
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2022-03-22 16:53:56 +0530

    Time: 4 ms (72.62%), Space: 6.7 MB (43.56%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        unordered_map<char, int> m;
+        for(int i=0;i<s.size();i++)
+            m[s[i]]=i;
+        int prev=0,cur=0;
+        vector<int> ans;
+        for(int i=0;i<s.size();i++){
+            cur=max(cur, m[s[i]]);
+            if(i==cur){
+                ans.push_back(i-prev+1);
+                prev=i+1;
+            }
+        }
+        return ans;
+    }
+};
