2051d489f419262b28c758de333f9ed3c9505824
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2022-03-25 21:05:12 +0530

    Time: 807 ms (36.72%), Space: 146.5 MB (43.79%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -2,25 +2,21 @@ class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
         
-        int ind=-1, mn=INT_MAX;
+        int ind=-1;
         
         for(int i=0;i<triplets.size();i++)
             if(triplets[i][0]==target[0]&&triplets[i][1]<=target[1]&&triplets[i][2]<=target[2])
                 ind=i;
-        
-        // cout<<"ind = "<<ind<<endl;
-        
+                
         if(ind==-1)
             return 0;
         
-        int ind1=-1, mn1=INT_MAX;
+        int ind1=-1;
         
         for(int i=0;i<triplets.size();i++)
             if(triplets[i][1]==target[1]&&triplets[i][0]<=target[0]&&triplets[i][2]<=target[2])
                 ind1=i;
-        
-        // cout<<"ind1 = "<<ind1<<endl;
-        
+                
         if(ind1==-1)
             return 0;
         
@@ -29,9 +25,7 @@ public:
         for(int i=0;i<triplets.size();i++)
             if(triplets[i][2]==target[2]&&triplets[i][0]<=target[0]&&triplets[i][1]<=target[1])
                 ind2=i;
-        
-        // cout<<"ind2 = "<<ind2<<endl;
-        
+                
         if(ind2==-1)
             return 0;
         
