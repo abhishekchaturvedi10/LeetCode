6fce2da4bf3ae2fd0cb6f4db590e42df1da0a5ba
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2022-03-25 16:16:57 +0530

    Time: 21 ms (50.85%), Space: 10.5 MB (53.46%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
     
-    static bool cmp(vector<int> v1, vector<int> v2) {
-        
-        if(v2[1]==v1[1])
-            return v2[2]>v1[2];
-        
-        return v2[1]>v1[1];
-    }
-    
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-     
-        sort(trips.begin(), trips.end(), cmp);
         
-        int total=0;
-        
-        unordered_map<int,int> removed;
+        vector<int> net(1005,0);
         
         for(int i=0;i<trips.size();i++) {
                         
-            for(int j=0;j<i;j++) {
-                
-                if(trips[j][2]<=trips[i][1]&&removed[j]==0) {
-                    
-                    total-=trips[j][0];
-                    
-                    removed[j]=1;
-                }
-            }
-            
-            total+=trips[i][0];
-            
+            net[trips[i][1]] += trips[i][0];
+            net[trips[i][2]] -= trips[i][0];
+        }
+        
+        int total=0;
+        
+        for(int i=0;i<=1000;i++) {
+            total += net[i];
             if(total>capacity)
                 return 0;
         }
