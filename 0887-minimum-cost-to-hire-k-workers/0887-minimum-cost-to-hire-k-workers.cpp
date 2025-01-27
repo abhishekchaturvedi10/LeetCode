c94a409d518d07d46f858c617d430ab4354a2dc8
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2025-01-27 11:10:38 +0530

    Time: 15 ms (41.01%), Space: 31.7 MB (55.92%) - LeetHub

---
+++ 0887-minimum-cost-to-hire-k-workers/0887-minimum-cost-to-hire-k-workers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& q, vector<int>& w, int k) {
+        
+        int n=q.size();
+        vector<pair<double,int>> rate;
+        for(int i=0;i<n;i++) {
+            rate.push_back({w[i]*1.0/q[i],q[i]});
+        }
+        sort(rate.begin(),rate.end());
+
+        double currTotalQuality = 0;
+        double totalCost = 1e12;
+        
+        priority_queue<int> workers;
+        for(int i=0;i<n;i++) {
+            workers.push(rate[i].second);
+            currTotalQuality += rate[i].second;
+            if(workers.size()>k) {
+                currTotalQuality -= workers.top();
+                workers.pop();
+            }
+            if(workers.size()==k) {
+                totalCost = min(totalCost,currTotalQuality*rate[i].first);
+            }
+        }
+        return totalCost;
+    }
+};
