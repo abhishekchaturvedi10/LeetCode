a5b6f08621e4f59d93f371e03ae282eec2380a31
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2025-01-27 13:31:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1499-maximum-performance-of-a-team/1499-maximum-performance-of-a-team.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxPerformance(int n, vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<pair<int,int>> v;
+        for(int i=0;i<n;i++) {
+            v.push_back({nums2[i],nums1[i]});
+        }
+        sort(v.begin(),v.end(),greater<>());
+        priority_queue<int,vector<int>,greater<int>> pq;
+        long long int curSum=0, res=0;
+        for(auto& x:v) {
+            pq.push(x.second);
+            curSum+=x.second;
+            if(pq.size()>k) {
+                curSum-=pq.top();
+                pq.pop();
+            }
+            if(pq.size()==k) {
+                res=max(res,curSum*x.first);
+            }
+        }
+        return res;
+    }
+};
