7df7695c2528b316d5f47325acc8b2d42f407a84
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2025-01-28 14:32:45 +0530

    Time: 13 ms (44.8%), Space: 11.5 MB (62.93%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        
-        unordered_map<char,int> freq;
-        
-        for(int i=0;i<t.size();i++)
-            freq[t[i]]++;
-                
-        int l=0,r=0,res=1e7,si=-1;
-            
-        while(r<s.size()) {
-            
-            freq[s[r]]--;
-            
-            while(l<=r&&freq[s[l]]<0)
-                freq[s[l++]]++;
-            
-            int f=0;
-                        
-            for(char i=0;i<=26;i++) {
-                if(freq[i+'a']>0 || freq[i+'A']>0) {
-                    f=1;
-                    break;
+
+        unordered_map<char, int> freq;
+
+        // Populate frequency map with characters from t
+        for (char c : t)
+            freq[c]++;
+
+        int l = 0, r = 0, res = INT_MAX, si = -1;
+        int required = t.size(); // Total characters from t that need to be matched
+
+        while (r < s.size()) {
+            // If character at 'r' is in 't', decrease its count and adjust required
+            if (freq[s[r]] > 0) {
+                required--;
+            }
+            freq[s[r]]--; // Decrease the count for the character at 'r'
+
+            // Shrink the window from the left if all characters are satisfied
+            while (required == 0) {
+                if (r - l + 1 < res) {
+                    res = r - l + 1;
+                    si = l;
                 }
+
+                // Prepare to remove the character at 'l' and update required
+                freq[s[l]]++;
+                if (freq[s[l]] > 0) {
+                    required++;
+                }
+                l++;
             }
-        
-            if(f==0&&(r-l+1)<res) {
-                res=r-l+1;
-                si=l;
-            }
-            
             r++;
         }
-        return si==-1?"":s.substr(si,res);
+
+        return si == -1 ? "" : s.substr(si, res);
     }
 };
