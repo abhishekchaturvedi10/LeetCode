3635f814804bb215c6209222c9df98e43e4f10af
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2025-01-28 15:46:29 +0530

    Time: 149 ms (5.02%), Space: 43.9 MB (5.02%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     map<pair<string,int>,bool> dp;
-    bool dfs(string& s, map<string,int>& m, string cur, int i) {
+    bool dfs(string& s, map<string,bool>& m, string cur, int i) {
         if(i==s.size()) {
-            if(m[cur]==1) {
-                return true;
-            } else {
-                return false;
-            }
+            return m[cur];
         }
         if(dp.find({cur,i})!=dp.end()) {
             return dp[{cur,i}];
         }
         if(m[cur]==1) {
-            string s1="";
-            s1+=s[i];
-            cur+=s[i];
-            return dp[{cur,i}]=dfs(s, m, s1,i+1) || dfs(s,m,cur,i+1);
+            return dp[{cur,i}]=dfs(s,m,string(1,s[i]),i+1)||dfs(s,m,cur+s[i],i+1);
         } else {
-            cur+=s[i];
-            return dp[{cur,i}]=dfs(s,m,cur,i+1);
+            return dp[{cur,i}]=dfs(s,m,cur+s[i],i+1);
         }
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        map<string,int> m;
+        map<string,bool> m;
         for(string s: wordDict) {
-            m[s]=1;
+            m[s]=true;
         }
         return dfs(s,m,"",0);
     }
