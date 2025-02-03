1e0ca721beacfc0a34df7f202468b64ac08d25cd
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2025-02-03 21:15:20 +0530

    Time: 2 ms (9.12%), Space: 27.7 MB (99.12%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -5,14 +5,15 @@ public:
         for(int i=1;i<n;i++) {
             if(nums[i]>nums[i-1]) {
                 inc++;
-            } else {
+                res=max(res,dec);
+                dec=1;
+            } else if (nums[i-1]>nums[i]) {
+                dec++;
                 res=max(res,inc);
                 inc=1;
-            }
-            if(nums[i-1]>nums[i]) {
-                dec++;
             } else {
-                res=max(res,dec);
+                res=max({res,inc,dec});
+                inc=1;
                 dec=1;
             }
         }
