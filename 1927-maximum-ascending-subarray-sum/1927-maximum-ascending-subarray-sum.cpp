8f73d5bc396dd565afbdcaae37f2584b95a83b2f
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2025-02-04 22:51:29 +0530

    Time: 0 ms (100%), Space: 11 MB (77.09%) - LeetHub

---
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int n=nums.size(), curSum=nums[0], res=0;
+        for(int i=1;i<n;i++) {
+            if(nums[i]>nums[i-1]) {
+                curSum+=nums[i];
+            } else {
+                res=max(res,curSum);
+                curSum=nums[i];
+            }
+        }
+        return max(res,curSum);
+    }
+};
