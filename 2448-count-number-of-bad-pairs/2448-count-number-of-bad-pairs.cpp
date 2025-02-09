32428d332e8d8533a02c41b68d5c81019b0ed8bb
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2025-02-09 14:41:08 +0530

    Time: 71 ms (47.87%), Space: 92.8 MB (9.57%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -5,9 +5,7 @@ public:
         int n=nums.size();
         long long res=0;
         for(int i=0;i<n;i++) {
-            if(i>0){
-                res+=(i-mp[nums[i]-i]);
-            }
+            res+=(i-mp[nums[i]-i]);
             mp[nums[i]-i]++;
         }
         return res;
