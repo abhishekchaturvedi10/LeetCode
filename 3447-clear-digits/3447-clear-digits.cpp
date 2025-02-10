dd03e33d1150849800b404369088fb109205e041
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2025-02-11 00:32:53 +0530

    Time: 0 ms (100%), Space: 8.5 MB (56.73%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        int n=s.size();
-        while(n>0) {
-            string x="";
-            bool found=false;
-            for(int i=0;i<n;i++) {
-                if(!found && isdigit(s[i])) {
-                    found=true;
-                    if(x.size()>0) {
-                        x.pop_back();
-                    }
-                } else {
-                    x+=s[i];
-                }
-            }
-            cout<<endl;
-            if(!found) {
-                return x;
+        string stack;
+        for (char c : s) {
+            if (isdigit(c)) {
+                if (!stack.empty()) stack.pop_back();
             } else {
-                n=x.size();
-                s=x;
+                stack.push_back(c);
             }
         }
-        return "";
+        return stack;
     }
 };
