65baf7d9cad6dfcb98e4723db2f82658e4138333
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2025-02-11 19:59:44 +0530

    Time: 5 ms (5.85%), Space: 12.6 MB (5.32%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -4,10 +4,7 @@ public:
         string st="";
         for(char& c:s) {
             st.push_back(c);
-            if(st.size()<part.size()) {
-                continue;
-            }
-            if(part==st.substr(st.size()-part.size())) {
+            if(st.size()>=part.size()&&part==st.substr(st.size()-part.size())) {
                 st = st.substr(0,st.size()-part.size());
             }
         }
