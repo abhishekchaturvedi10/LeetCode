72dcf6fe64fcb833155d69e85b4b28927cfc0317
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2025-02-20 22:48:43 +0530

    Time: 0 ms (100%), Space: 12.6 MB (84%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
-    map<pair<string,int>,string> seen;
-    unordered_map<string,int> seen1;
-    string rec(int i, int n, string& cur) {
-        if(i>n) {
-            return "";
-        }
-        if(i==n) {
-            if(seen1.find(cur)!=seen1.end() ) {
-                return "";
-            } else {
-                return cur;
-            }
-        }
-        if(seen.find({cur,i})!=seen.end()) {
-            return seen[{cur,i}];
-        }
-        string cur1=cur+"0";
-        string x = rec(i+1,n,cur1);
-        if(x!="") {
-            return seen[{cur,i}]=x;
-        }
-        cur1=cur+"1";
-        string y = rec(i+1,n,cur1);
-        return seen[{cur,i}]=y;
-    }
     string findDifferentBinaryString(vector<string>& nums) {
-        int n=nums.size();
-        for(string& s:nums) {
-            seen1[s]=1;
+        string ans = string(nums.size(), '0');  // Start with "000...0"
+        
+        if(find(nums.begin(), nums.end(), ans) == nums.end()) return ans;  // Check if it already works
+
+        for(int i = 0; i < nums.size(); i++) { 
+            ans[i] = '1';  // Try flipping one bit at a time
+            if(find(nums.begin(), nums.end(), ans) == nums.end()) 
+                return ans;  
+            ans[i] = '0';  // Revert back if needed
         }
-        string cur="";
-        return rec(0, nums.size(), cur);
+        return ans;  // Fallback case
     }
 };
