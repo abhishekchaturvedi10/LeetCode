af6deddde1fdf0f28becd0d422e16df307214e31
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2025-02-28 00:59:14 +0530

    Time: 266 ms (82.05%), Space: 15.5 MB (88.68%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& arr) {
-        if (arr.size() <= 2) return 0;
-
         int n = arr.size();
         unordered_map<int, int> mp;
         for (int i = 0; i < n; i++) {
-            mp[arr[i]] = i;
+            mp[arr[i]]=1;
         }
-
         int maxi = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
