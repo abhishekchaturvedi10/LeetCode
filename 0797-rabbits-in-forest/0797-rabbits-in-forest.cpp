4100997ffec22a09fbf6137d5ae7d02062f272a2
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2025-04-20 10:58:39 +0530

    Time: 4 ms (9.32%), Space: 12.2 MB (42.46%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -7,16 +7,12 @@ public:
         }
         int res=0;
         for(auto p:mp) {
-            if(p.first==0) {
-                res+=mp[0];
-            } else {
-                int x=p.first+1;
-                int y=p.second/x;
-                int z=p.second%x;
-                res+=x*y;
-                if(z!=0) {
-                    res+=x;
-                }
+            int x=p.first+1;
+            int y=p.second/x;
+            int z=p.second%x;
+            res+=x*y;
+            if(z!=0) {
+                res+=x;
             }
         }
         return res;
