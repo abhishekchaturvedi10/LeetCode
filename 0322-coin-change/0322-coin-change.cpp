03a5f75a8d735521dce9946dfd67edbaf3f4c3e0
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-02-14 16:23:08 +0530

    Time: 15 ms (92.2%), Space: 17.8 MB (92.88%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> dp(amount+1,1000000);
         dp[0]=0;
         for(int i=0;i<amount;i++) {
-            for(int c:coins) {
+            for(int& c:coins) {
                 if(c<=amount&&i+c<=amount) {
                     dp[i+c]=min(dp[i+c],dp[i]+1);
                 }
