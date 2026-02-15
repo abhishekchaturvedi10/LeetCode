9a8f052a965945d2cf61ac2534c75b0cd67af5f3
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-02-16 00:54:25 +0530

    Time: 7 ms (20.46%), Space: 18.3 MB (26.63%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     struct sorter{
-        bool operator()(pair<int,string> const& p1, pair<int,string> const& p2){
+        bool operator() (pair<int,string> p1, pair<int,string> p2){
             if(p1.first==p2.first)
                 return p1.second<p2.second;
             return p1.first>p2.first;
@@ -12,12 +12,10 @@ public:
     vector<string> topKFrequent(vector<string>& words, int k) {
      
         map<string,int> freq;
-        
         for(auto s:words)
             freq[s]++;
         
         priority_queue<pair<int,string>, vector<pair<int,string>>, sorter> pq;
-        
         for(auto &i:freq) {
             pq.push({i.second,i.first});
             if(pq.size()>k) {
@@ -27,7 +25,6 @@ public:
         
         vector<string> res(k);
         int m=k-1;
-        
         while(pq.size()){
             auto p=pq.top();
             pq.pop();
