ea36b3701148566514f2ddf7f29039d119bdaee6
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-02-16 00:54:45 +0530

    Time: 3 ms (64.45%), Space: 18.2 MB (26.63%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     struct sorter{
-        bool operator() (pair<int,string> p1, pair<int,string> p2){
+        bool operator() (pair<int,string>& p1, pair<int,string>& p2){
             if(p1.first==p2.first)
                 return p1.second<p2.second;
             return p1.first>p2.first;
