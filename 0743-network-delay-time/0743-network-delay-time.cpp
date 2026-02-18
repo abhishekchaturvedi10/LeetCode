507ccfda1d73b6dc350e5e7a3eca93dedcd08c9d
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-02-18 11:41:55 +0530

    Time: 154 ms (5.03%), Space: 76.9 MB (5.03%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        
+        vector<vector<vector<int>>> graph(n+1);
+
+        for(vector<int>& t:times) {
+            int u=t[0], v=t[1], w=t[2];
+            graph[u].push_back({v,w});
+        }
+
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+        pq.push({0,k});
+
+        vector<int> distance(n+1, INT_MAX);
+        distance[k]=0;
+
+        while(pq.size()) {
+            vector<int> topElement = pq.top();
+            pq.pop();
+            int u = topElement[1], dist = topElement[0];
+            auto neighbors = graph[u];
+            for(auto& neighbor: neighbors) {
+                int v = neighbor[0], weight = neighbor[1];
+                int curDist = dist + weight;
+                if(distance[v] > curDist) {
+                    pq.push({curDist, v});
+                    distance[v] = curDist;
+                }
+            }
+        }
+
+        int maxTime = -1;
+        for(int i=1;i<=n;i++) {
+            maxTime = max(maxTime,distance[i]);
+        }
+
+        return maxTime == INT_MAX ? -1:maxTime;
+    }
+};
