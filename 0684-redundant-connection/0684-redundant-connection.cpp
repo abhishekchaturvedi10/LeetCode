a8fc20d48da298a8cd7633113fcbfab6bd076b76
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-02-20 23:55:11 +0530

    Time: 0 ms (100%), Space: 12.8 MB (82.68%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -12,11 +12,11 @@ public:
         int parent[edges.size()+1];
         memset(parent,-1,sizeof(parent));
         for(auto i:edges){
-            int x=find(parent, i[0]),y=find(parent, i[1]);
+            int x=find(parent,i[0]),y=find(parent,i[1]);
             if(x==y)
                 return i;
             else
-                Union(parent, x, y);
+                Union(parent,x,y);
         }
         return edges[0];
     }
