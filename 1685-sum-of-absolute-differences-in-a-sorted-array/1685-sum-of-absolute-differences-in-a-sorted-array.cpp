733a32bfffb4e0c6c953ca2c2f68f9fbde504dc2
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-02-21 15:45:15 +0530

    Time: 555 ms (5.11%), Space: 89.6 MB (57.29%) - LeetHub

---
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> preSum(n), res(n);
+        preSum[0]=nums[0];
+
+        for(int i=1;i<n;i++) {
+            preSum[i] = preSum[i-1]+nums[i];
+        }
+
+        for(int i=0;i<n;i++) {
+            int prevSum = i>0?((nums[i]*i)-preSum[i-1]):0;
+            int nextSum = i<n-1?((preSum[n-1]-preSum[i])-(nums[i]*(n-i-1))):0;
+            cout<<i<<" "<<prevSum<<" "<<nextSum<<endl;
+            res[i]=prevSum+nextSum;
+        }
+
+        return res;
+    }
+};
