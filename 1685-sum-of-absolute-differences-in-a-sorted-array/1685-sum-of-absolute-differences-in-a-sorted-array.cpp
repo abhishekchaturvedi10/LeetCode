7f9c326496a54097064d238d4cad2d4f2cffee08
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-02-21 15:45:21 +0530

    Time: 97 ms (5.11%), Space: 89.7 MB (46.11%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -13,7 +13,6 @@ public:
         for(int i=0;i<n;i++) {
             int prevSum = i>0?((nums[i]*i)-preSum[i-1]):0;
             int nextSum = i<n-1?((preSum[n-1]-preSum[i])-(nums[i]*(n-i-1))):0;
-            cout<<i<<" "<<prevSum<<" "<<nextSum<<endl;
             res[i]=prevSum+nextSum;
         }
 
