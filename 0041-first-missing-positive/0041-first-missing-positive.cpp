8da2869b0ed83fd7d0dc3e5c613a3681f1d079b5
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-02-21 18:55:15 +0530

    Time: 2 ms (56.59%), Space: 55.1 MB (62.91%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -11,8 +11,9 @@ public:
         }
         
         for(int i=0;i<n;i++) {
-            if(abs(nums[i])<=n && nums[abs(nums[i])-1]>0) {
-                nums[abs(nums[i])-1]*=-1;
+            int x = abs(nums[i]);
+            if(x<=n && nums[x-1]>0) {
+                nums[x-1]*=-1;
             }
         }
         
