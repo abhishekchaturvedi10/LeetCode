f69e4ddeaaac017db51b3518d9a5ead5a1fc74e8
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-02-23 00:55:11 +0530

    Time: 2134 ms (5.21%), Space: 100.9 MB (15.61%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        
+        long long subarraySum=0, res=0;
+        int cnt=0;
+        map<int,int> freq;
+        
+        for(int i=0;i<k;i++) {
+            subarraySum+=nums[i];
+            freq[nums[i]]++;
+            if(freq[nums[i]]==2) {
+                cnt++;
+            }
+        }
+
+        cnt=max(cnt,0);
+        if(cnt==0) {
+            res=subarraySum;
+        }
+
+        int l=1, r=k;
+        while(r<nums.size()) {
+            cout<<l<<" "<<r<<" "<<subarraySum<<" "<<cnt<<" ";
+            subarraySum-=nums[l-1];
+            subarraySum+=nums[r];
+            freq[nums[l-1]]--;
+            if(freq[nums[l-1]]==1) {
+                cnt--;
+            }
+            freq[nums[r]]++;
+            if(freq[nums[r]]==2) {
+                cnt++;
+            }
+            cnt=max(cnt,0);
+            if(cnt==0) {
+                res=max(res,subarraySum);
+            }
+            r++;
+            l++;
+        }
+
+        cnt=max(cnt,0);
+        if(cnt==0) {
+            res=max(res,subarraySum);
+        }
+
+        return res;
+    }
+};
