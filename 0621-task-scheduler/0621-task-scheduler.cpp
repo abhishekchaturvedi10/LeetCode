5c04da4bb57e26fa761e2c3914e16752bc718180
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-02-23 14:11:22 +0530

    Time: 0 ms (100%), Space: 38.3 MB (68.78%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int freq[26] = {0};
-        for(char task : tasks){
-            freq[task - 'A']++;
-        }
-        sort(begin(freq) , end(freq));
-        int chunk = freq[25] - 1;
-        int idel = chunk * n;
+        
+        vector<int> freq(26);
 
-        for(int i=24; i>=0; i--){
-            idel -= min(chunk,freq[i]);
+        for(char c:tasks) {
+            freq[c-'A']++;
         }
 
-        return idel < 0 ? tasks.size() : tasks.size() + idel;
+        sort(begin(freq),end(freq));
+        int chunk = freq[25]-1;
+        int idle = n*(freq[25]-1);
+        
+
+        for(int i=24;i>=0;i--) {
+            idle -= min(chunk,freq[i]);
+        }
+
+        return idle<0?tasks.size():tasks.size()+idle;
     }
 };
