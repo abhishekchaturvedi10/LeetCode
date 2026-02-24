a701d02c30b8372466db6387be761ba3951b5a52
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-02-24 13:05:22 +0530

    Time: 0 ms (100%), Space: 10.5 MB (63.15%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         
-        queue<pair<int,int>> q;
-        int time=0, kthPersonTickets;
+        int time=0;
 
         for(int i=0;i<tickets.size();i++) {
             if(i<=k) {
@@ -12,30 +11,6 @@ public:
                 time += min(tickets[i],tickets[k]-1);
             }
         }
-
-        // while(!q.empty()) {
-        //     pair<int,int> firstPerson = q.front();
-        //     q.pop();
-        //     int ticketOfFirstPerson = firstPerson.first, index=firstPerson.second;
-        //     if(index<=k) {
-        //         time += min(ticketOfFirstPerson,kthPersonTickets);
-        //     } else {
-        //         time += min(ticketOfFirstPerson,kthPersonTickets-1);
-        //     }
-        //     // 1,2,3,4  k=1
-
-        //     // time++;
-        //     // int ticketOfFirstPerson = firstPerson.first;
-        //     // if(firstPerson.second==1) {
-        //     //     if(ticketOfFirstPerson==1) {
-        //     //         return time;
-        //     //     }
-        //     // }
-        //     // ticketOfFirstPerson--;
-        //     // if(ticketOfFirstPerson>0) {
-        //     //     q.push({ticketOfFirstPerson,firstPerson.second});
-        //     // }
-        // }
 
         return time;
     }
