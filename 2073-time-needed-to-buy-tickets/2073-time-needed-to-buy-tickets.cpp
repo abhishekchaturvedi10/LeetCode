36821b069eb8c92abedd4ff61c8963268cc0af1b
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-02-24 12:55:43 +0530

    Time: 0 ms (100%), Space: 10.4 MB (82.49%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -3,31 +3,39 @@ public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         
         queue<pair<int,int>> q;
-        int time=0;
+        int time=0, kthPersonTickets;
 
         for(int i=0;i<tickets.size();i++) {
-            if(i==k) {
-                q.push({tickets[i],1});
+            if(i<=k) {
+                time += min(tickets[i],tickets[k]);
             } else {
-                q.push({tickets[i],0});
+                time += min(tickets[i],tickets[k]-1);
             }
         }
 
-        while(!q.empty()) {
-            pair<int,int> firstPerson = q.front();
-            q.pop();
-            time++;
-            int ticketOfFirstPerson = firstPerson.first;
-            if(firstPerson.second==1) {
-                if(ticketOfFirstPerson==1) {
-                    return time;
-                }
-            }
-            ticketOfFirstPerson--;
-            if(ticketOfFirstPerson>0) {
-                q.push({ticketOfFirstPerson,firstPerson.second});
-            }
-        }
+        // while(!q.empty()) {
+        //     pair<int,int> firstPerson = q.front();
+        //     q.pop();
+        //     int ticketOfFirstPerson = firstPerson.first, index=firstPerson.second;
+        //     if(index<=k) {
+        //         time += min(ticketOfFirstPerson,kthPersonTickets);
+        //     } else {
+        //         time += min(ticketOfFirstPerson,kthPersonTickets-1);
+        //     }
+        //     // 1,2,3,4  k=1
+
+        //     // time++;
+        //     // int ticketOfFirstPerson = firstPerson.first;
+        //     // if(firstPerson.second==1) {
+        //     //     if(ticketOfFirstPerson==1) {
+        //     //         return time;
+        //     //     }
+        //     // }
+        //     // ticketOfFirstPerson--;
+        //     // if(ticketOfFirstPerson>0) {
+        //     //     q.push({ticketOfFirstPerson,firstPerson.second});
+        //     // }
+        // }
 
         return time;
     }
