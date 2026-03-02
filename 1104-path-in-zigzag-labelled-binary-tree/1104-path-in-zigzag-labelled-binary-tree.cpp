f3541efdf3a8634a14c525e87768697ba41bbbb6
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-03-02 18:01:54 +0530

    Time: 0 ms (100%), Space: 8.5 MB (63.16%) - LeetHub

--- 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
@@ -1,56 +1,20 @@
 class Solution {
 public:
+    vector<int> pathInZigZagTree(int label) {
 
-    
-
-    vector<int> pathInZigZagTree(int& label) {
+        vector<int> result;
+        result.push_back(label); 
+        int height=log2(label); 
         
-        unordered_map<int,int> par;
-        par[1]=-1;
-        queue<int> q;
-        q.push(1);
-        int level=1;
-        bool labelFound=false;
-
-        while(!labelFound) {
-            level++;
-            int startVal=pow(2,level-1), endVal=pow(2,level)-1;
-            int n=q.size();
-            while(n--) {
-                int x=q.front();
-                q.pop();
-                if(x==label) {
-                    labelFound=true;
-                    break;
-                }
-                if(level&1) {
-                    q.push(startVal);
-                    par[startVal]=x;
-                    startVal++;
-                    q.push(startVal);
-                    par[startVal]=x;
-                    startVal++;
-                } else {
-                    q.push(endVal);
-                    par[endVal]=x;
-                    endVal--;
-                    q.push(endVal);
-                    par[endVal]=x;
-                    endVal--;
-                }
-            }
+        while(height!=0){
+            int left=pow(2,height-1);
+            int right=pow(2,height)-1;
+            label=left+(right-label/2);
+            result.push_back(label);
+            height--;
         }
-
-        vector<int> res;
-        res.push_back(label);
-
-        while(par[label]!=-1) {
-            res.push_back(par[label]);
-            label=par[label];
-        }
-
-        reverse(res.begin(),res.end());
-
-        return res;
+        
+        reverse(result.begin(),result.end());
+        return result;
     }
 };
