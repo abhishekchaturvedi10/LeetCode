f0f148b573a5024c64675f4cbb1eab74c797845b
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-03-04 08:29:31 +0530

    Time: 111 ms (14.44%), Space: 132.5 MB (10.69%) - LeetHub

--- 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.cpp
+++ 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.cpp
@@ -9,50 +9,36 @@ public:
             s2+=n2;
         }
         diff=abs(s1-s2);
-        priority_queue<int> h1, h2;
+        vector<int> diffs;
         for(int& n1:nums1) {
             if(s1<s2) {
                 if(n1<6) {
-                    h1.push(6-n1);
+                    diffs.push_back(6-n1);
                 }
             } else {
                 if(n1>1) {
-                    h1.push(n1-1);
+                    diffs.push_back(n1-1);
                 }
             }
         }
         for(int& n2:nums2) {
             if(s1>s2) {
                 if(n2<6) {
-                    h2.push(6-n2);
+                    diffs.push_back(6-n2);
                 }
             } else {
                 if(n2>1) {
-                    h2.push(n2-1);
+                    diffs.push_back(n2-1);
                 }
             }
         }
-        while(!h1.empty()||!h2.empty()) {
+        sort(diffs.rbegin(),diffs.rend());
+        for(int& x:diffs) {
             if(diff<=0) {
-                return res;
+                break;
             }
+            diff-=x;
             res++;
-            if(!h1.empty()&&!h2.empty()) {
-                if(h1.top()>h2.top()) {
-                    diff-=h1.top();
-                    h1.pop();
-                } else {
-                    diff-=h2.top();
-                    h2.pop();
-                }
-            } else if(!h1.empty()) {
-                diff-=h1.top();
-                h1.pop();
-            } else {
-                cout<<h2.top()<<endl;
-                diff-=h2.top();
-                h2.pop();
-            }
         }
         return diff<=0?res:-1;
     }
