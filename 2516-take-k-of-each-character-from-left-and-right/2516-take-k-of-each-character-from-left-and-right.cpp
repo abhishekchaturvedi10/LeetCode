029ecb61301b8dc0fa981cc50382405c7b6a16b6
Abhishek Chaturvedi <abhishekchaturvedi10oct@gmail.com>
2026-03-14 11:21:49 +0530

    Time: 16 ms (50%), Space: 12.3 MB (97.72%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -1,45 +1,35 @@
 class Solution {
 public:
     int takeCharacters(string& s, int k) {
+
+        vector<int> freq(3, 0);
+        int n = s.length();
         
-        int aCnt=0, bCnt=0, cCnt=0, mins=0, l=0, r=0, totalACnt=0, totalBCnt=0, totalCCnt=0;
-
-        for(char& c:s) {
-            if(c=='a') {
-                totalACnt++;
-            } else if(c=='b') {
-                totalBCnt++;
-            } else {
-                totalCCnt++;
-            }
+        for (char& c : s) {
+            freq[c - 'a']++;
         }
-
-        if(totalACnt<k || totalBCnt<k || totalCCnt<k) {
+        
+        if (freq[0] < k || freq[1] < k || freq[2] < k) {
             return -1;
         }
-
-        while(r<s.size()) {
-            if(s[r]=='a') {
-                aCnt++;
-            } else if(s[r]=='b') {
-                bCnt++;
-            } else {
-                cCnt++;
+        
+        vector<int> curr(3, 0);
+        int maxLen = 0;
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            curr[s[right] - 'a']++;
+            
+            while (left <= right && (curr[0] > freq[0] - k || 
+                   curr[1] > freq[1] - k || 
+                   curr[2] > freq[2] - k)) {
+                curr[s[left] - 'a']--;
+                left++;
             }
-            while(l<=r && (totalACnt-aCnt<k || totalBCnt-bCnt<k || totalCCnt-cCnt<k)) {
-                if(s[l]=='a') {
-                    aCnt--;
-                } else if(s[l]=='b') {
-                    bCnt--;
-                } else {
-                    cCnt--;
-                }
-                l++;
-            }
-            mins=max(mins,r-l+1);
-            r++;
+            
+            maxLen = max(maxLen, right - left + 1);
         }
-
-        return s.size()-mins;
+        
+        return n - maxLen;
     }
 };
